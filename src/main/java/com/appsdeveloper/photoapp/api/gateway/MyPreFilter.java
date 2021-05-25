c1196a98b99d0ba96fec1adbae9e743e44ecf8bc
Elmar Angao <32320010+eangao@users.noreply.github.com>
2021-05-25 18:28:02 +0800

    Creating Global Pre Filter - closes # 16

---
+++ src/main/java/com/appsdeveloper/photoapp/api/gateway/MyPreFilter.java
@@ -0,0 +1,26 @@
+package com.appsdeveloper.photoapp.api.gateway;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+
+
+@Component
+public class MyPreFilter implements GlobalFilter {
+
+    final Logger logger = LoggerFactory.getLogger(MyPreFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        logger.info("My first Pre-filter is executed...");
+
+        return chain.filter(exchange);
+    }
+}
